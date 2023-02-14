6b4ef7ba48c227e2f91345f7048d71973ec77d88
Nick Ronnei <nronnei@gmail.com>
2023-02-14 12:11:47 -0500

    Fixes gap between AppBar and Map

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChakraProvider, Box, HStack, useDisclosure } from '@chakra-ui/react';
+import { ChakraProvider, Flex, Box, useDisclosure } from '@chakra-ui/react';
 import React from 'react';
 import { AppBar } from './components/AppBar';
 import { AppDrawer } from './components/AppDrawer';
@@ -9,11 +9,13 @@ export const App = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <ChakraProvider>
-      <HStack spacing={0}>
-        <AppBar onOpen={onOpen}></AppBar>
-        <AppDrawer isOpen={isOpen} onClose={onClose}></AppDrawer>
+      <Flex direction="row">
+        <Box>
+          <AppBar onOpen={onOpen}></AppBar>
+          <AppDrawer isOpen={isOpen} onClose={onClose}></AppDrawer>
+        </Box>
         <Map />
-      </HStack>
+      </Flex>
     </ChakraProvider>
   )
 };
